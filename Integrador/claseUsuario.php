94dbb7feef9ed1fa5f4b88a0cbbb46a6ebeec3f3
JeronimoFabio <jeronimo.fabio@hotmail.com>
2018-07-08 17:37:17 -0300

    hice la clase usuario.

---
+++ Integrador/claseUsuario.php
@@ -0,0 +1,58 @@
+<?php 
+
+class Usuario{
+
+ protected $nombreUsuario = [];
+ protected $apellidoUsuario = [];
+ private $contrasenaUsuario = [];
+ protected $emailUsuario = [];
+ 
+public function __construct(array $nombreUsuario,array $apellidoUsuario,array $contrasenaUsuario,array $emailUsuario){
+
+ $this->nombreUsuario = $nombreUsuario;
+ $this->apellidoUsuario = $apellidoUsuario;
+ $this->contrasenaUsuario = $contrasenaUsuario;
+ $this->emailUsuario = $emailUsuario;
+}
+
+
+ public function setNombreUsuario($nombre){
+ 	$this->nombreUsuario = $nombre;
+ }
+
+ public function getNombreUsuario(){
+ 	return $nombreUsuario;
+ }
+
+ public function setApellidoUsuario($apellido){
+ 	$this->nombreUsuario = $apellido;
+ }
+
+ public function getApellidoUsuario(){
+ 	return $apellidoUsuario;
+ }
+
+  public function setEmail($emailUsuario){
+ 	$this->emailUsuario = $emailUsuario;
+ }
+
+ public function getEmail(){
+ 	return $emailUsuario;
+ }
+
+ 
+ }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>

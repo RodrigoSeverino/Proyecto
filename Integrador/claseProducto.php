7ec7fcc7078f922a33ae76814ccc006903b5251d
JeronimoFabio <jeronimo.fabio@hotmail.com>
2018-07-20 18:49:49 -0300

    hice produto y usuario, me faltanm los otros

--- Integrador/claseProducto.php
+++ Integrador/claseProducto.php
@@ -1,4 +1,73 @@
+<?php  
 
+
+class Producto {
+
+   protected $nombreProducto = [];
+   protected $idProducto = [];
+   protected $precioProducto = [];
+   protected $fotoProducto = [];
+   protected $stockProducto = [];
+
+
+  public function __construct(array $nombreProducto,array $idProducto,array $precioProducto,array $fotoProducto,array $stockProducto){
+
+   
+ $this->nombreProducto = $nombreProducto;
+ $this->idProducto = $idProducto;
+ $this->precioProducto = $precioProducto;
+ $this->fotoProducto = $fotoProducto;
+ $this->stockProducto= $stockProducto;
+  }
+
+public function listarProductos(){
+
+}
+
+public function traerProductosId(){
+
+}
+
+
+public function traerProductosNombre(){
+
+}
+
+public function traerStock(){
+
+}
+
+public function comprarse(){
+
+}
+
+
+public function descuento(){
+
+}
+
+
+public function promociones(){
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
 
 
 

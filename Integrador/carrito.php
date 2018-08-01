9f6d2a5d591e6626c73cbbfc0354936cf03a60d5
RodrigoSeverino <rodrigoseverino.103@gmail.com>
2018-07-31 23:54:37 -0300

    css parte 2

--- Integrador/carrito.php
+++ Integrador/carrito.php
@@ -5,7 +5,7 @@
   <title>Carrito</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=devise-width, initial-scale=1">
-  <link rel="stylesheet"  href="estiloscss/serviciotecnico.css">
+  <link rel="stylesheet"  href="estiloscss/contacto.css">
   <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
 </head>
 <body>
